da7891c16468dc354ef6022388d408a986dad793
JoseMGT-31 <bekandev31@gmail.com>
2025-11-25 01:56:59 -0300

    Actualizar origen permitido en CORS para producción

--- main.py
+++ main.py
@@ -13,9 +13,10 @@ app.mount("/images", StaticFiles(directory=images_dir), name="images")
 
 app.include_router(user.router)
 app.include_router(product.router)
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://ec2-44-214-169-188.compute-1.amazonaws.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
